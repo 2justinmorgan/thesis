34d6245157a1cf7babf32abeb82eef571c2d1c11
Justin_Morgan <2justinmorgan@gmail.com>
2020-10-16 13:20:54 -0700

    Implemented theta feature function BRANCH: research/mouse_data_feature_engineering

--- src/generate_mouse_movement_features.py
+++ src/generate_mouse_movement_features.py
@@ -1,3 +1,10 @@
 
+import math
+
 def hello(name):
 	return "hello " + str(name)
+
+def theta(pointA, pointB):
+	deltaX = abs(pointA[0] - pointB[0])
+	deltaY = abs(pointA[1] - pointB[1])
+	return math.atan2(deltaY, deltaX)
--- test/test_generate_mouse_movement_features.py
+++ test/test_generate_mouse_movement_features.py
@@ -3,3 +3,12 @@ import generate_mouse_movement_features as gen_features
 
 def test_hello():
 	assert gen_features.hello("Mike") == "hello Mike"
+
+@pytest.mark.parametrize(
+	"pointA,pointB,expect",
+	[
+		([23,42],[31,45],pytest.approx(0.35877067027057225)),
+		([1020,355],[1003,361],pytest.approx(0.3392926144540447))
+	])
+def test_theta(pointA, pointB, expect):
+	assert gen_features.theta(pointA, pointB) == expect
