4950974b876aee31e81d555bea10fbb9a3f31ae4
Justin_Morgan <2justinmorgan@gmail.com>
2020-10-16 19:23:58 -0700

    Added a few simple test cases for all velocity functions BRANCH: research/mouse_data_feature_engineering

--- test/test_generate_mouse_movement_features.py
+++ test/test_generate_mouse_movement_features.py
@@ -16,8 +16,10 @@ def test_theta(pointA, pointB, expect):
 @pytest.mark.parametrize(
 	"axis,tpointA,tpointB,expect",
 	[
+		('x',[0,0,0],[1,0,1],pytest.approx(1)),
 		('x',[487,912,3419.371],[488,915,3419.62],pytest.approx(4.016064257)),
 		('x',[50,75,0.001],[55,76,0.092],pytest.approx(54.94505495)),
+		('y',[0,2,0],[1,4,1],pytest.approx(2)),
 		('y',[985,102,40.95],[993,94,41],pytest.approx(160)),
 		('y',[0,4,0.00005],[3,2,0.0038],pytest.approx(533.333333333))
 	])
@@ -27,6 +29,7 @@ def test_axis_velocity(axis, tpointA, tpointB, expect):
 @pytest.mark.parametrize(
 	"tpointA,tpointB,expect",
 	[
+		([10,20,1],[15,32,2],pytest.approx(13)),
 		([56,341,15290.04],[57,341,15290.29],pytest.approx(4.0)),
 		([1490,888,0.92],[1539,831,1.005],pytest.approx(884.3115516689962)),
 	])
