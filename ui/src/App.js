2adcde034f2ee7e0f80ff5d04768437e6f6cc179
Justin_Morgan <2justinmorgan@gmail.com>
2021-01-06 21:53:28 -0800

    Initialized recharts starter code BRANCH: research/setup_data_visualization_ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  ZAxis,
+} from 'recharts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const data01 = React.useMemo(
+        () => require("./data").points.slice(0,10),
+        []
+    );
+
+    const [ data, setData ] = useState(data01);
+
+    return (
+        <ScatterChart width={500} height={400}>
+            <XAxis type="number" dataKey="x" />
+            <YAxis type="number" dataKey="y" />
+            <ZAxis range={[0]} />
+            <Scatter data={data} fill="green" lineJointType="natural" line />
+        </ScatterChart>
+    );
 }
 
 export default App;
+
