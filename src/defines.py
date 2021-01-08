6e53f7102b1cd5e04bf9e7fbe6ca4d918b3d50d5
Justin_Morgan <2justinmorgan@gmail.com>
2021-01-07 20:15:12 -0800

    Initialized defines py script BRANCH: research/mouse_data_feature_engineering

---
+++ src/defines.py
@@ -0,0 +1,32 @@
+
+FEATURES = [
+    "velocity",
+    "xvelocity",
+    "yvelocity",
+    "acceleration",
+    "jerk",
+    "theta"
+]
+
+METRICS = {
+    "all": [],
+    "stats": {
+        "stdev": 0.0,
+        "mean": 0.0,
+        "range": []
+    }
+}
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class TPoint(Point):
+    def __init__(self, x, y, time):
+        super().__init__(x, y)
+        self.x = int(x)
+        self.y = int(y)
+        self.time = float(time)
--- src/generate_mouse_movement_features.py
+++ src/generate_mouse_movement_features.py
@@ -4,39 +4,7 @@ import sys
 import copy
 import commons
 import formatout
-
-
-FEATURES = [
-    "velocity",
-    "xvelocity",
-    "yvelocity",
-    "acceleration",
-    "jerk",
-    "theta"
-]
-
-METRICS = {
-    "all": [],
-    "stats": {
-        "stdev": 0.0,
-        "mean": 0.0,
-        "range": []
-    }
-}
-
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-class TPoint(Point):
-    def __init__(self, x, y, time):
-        super().__init__(x, y)
-        self.x = int(x)
-        self.y = int(y)
-        self.time = float(time)
+from defines import FEATURES, METRICS, Point, TPoint
 
 
 def print_usage(exit_program):
