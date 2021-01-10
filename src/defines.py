96d901621226bf89ef9be4b604350eebd939c132
Justin_Morgan <2justinmorgan@gmail.com>
2021-01-10 11:36:01 -0800

    Implemented locker class BRANCH: research/mouse_data_feature_engineering

--- src/defines.py
+++ src/defines.py
@@ -22,46 +22,72 @@ METRICS = {
 }
 
 
-class Point:
-    def __init__(self, x, y):
+class Locker:
+    __locked = False
+
+    def __setattr__(self, key, val):
+        if self.__locked and not hasattr(self, key):
+            raise TypeError("%r does not allow setting new attributes after object instantiation" % self)
+        if key == '_Locker__locked' and val != True:
+            raise TypeError(f"Unable to Falsify the {key} attribute of %r instances" % self.__class__)
+        object.__setattr__(self, key, val)
+
+    def setlock(self, _inherited):
+        if not _inherited:
+            self._Locker__locked = True
+
+
+class Point(Locker):
+    def __init__(self, x, y, _inherited=False):
+        super().__init__()
         self.x = x
         self.y = y
+        self.setlock(_inherited)
 
 
-class TPoint(Point):
-    def __init__(self, x, y, time):
-        super().__init__(x, y)
+class TPoint(Point, Locker):
+    def __init__(self, x, y, time, _inherited=False):
+        super().__init__(x, y, _inherited=True)
         self.x = int(x)
         self.y = int(y)
         self.time = float(time)
+        self.setlock(_inherited)
 
 
-class Session:
-    def __init__(self, user="", id=""):
+class Session(Locker):
+    def __init__(self, user="", id="", _inherited=False):
+        super().__init__()
         self.user = user
         self.id = id
+        self.setlock(_inherited)
 
 
-class Range:
-    def __init__(self, low=0.0, high=0.0):
+class Range(Locker):
+    def __init__(self, low=0.0, high=0.0, _inherited=False):
+        super().__init__()
         self.low = low
         self.high = high
+        self.setlock(_inherited)
 
 
-class Stats:
-    def __init__(self, mean=0.0, median=0.0, mode=0.0, stdev=0.0, range=Range()):
+class Stats(Locker):
+    def __init__(self, mean=0.0, median=0.0, mode=0.0, stdev=0.0, range=Range(), _inherited=False):
+        super().__init__()
         self.mean = mean
         self.median = median
         self.mode = mode
         self.stdev = stdev
         self.range = range
+        self.setlock(_inherited)
 
 
-class Feature:
-    def __init__(self, name=""):
+class Feature(Locker):
+    def __init__(self, name="", _inherited=False):
+        super().__init__()
         self.name = name
         self.stats = Stats()
         self.records = []
+        self.setlock(_inherited)
 
     def add(self, record):
         self.records.append(record)
--- test/test_gen_features.py
+++ test/test_gen_features.py
@@ -53,7 +53,7 @@ def mock_record_features(tmpdir):
 def check_if_range_class_instance_has_all_member_variables(range_class_instance):
     obj = range_class_instance
     exected_members = ["low", "high"]
-    actual_members = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+    actual_members = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("_")]
     assert len(exected_members) == len(actual_members)
     for expected_member in exected_members:
         assert expected_member in actual_members
@@ -62,7 +62,7 @@ def check_if_range_class_instance_has_all_member_variables(range_class_instance)
 def check_if_stats_class_instance_has_all_member_variables(stats_class_instance):
     obj = stats_class_instance
     exected_members = ["mean", "median", "mode", "stdev", "range"]
-    actual_members = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+    actual_members = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("_")]
     assert len(exected_members) == len(actual_members)
     for expected_member in exected_members:
         assert expected_member in actual_members
@@ -71,7 +71,7 @@ def check_if_stats_class_instance_has_all_member_variables(stats_class_instance)
 def check_if_feature_class_instance_has_all_member_variables(feature_class_instance):
     obj = feature_class_instance
     exected_members = ["name", "stats", "records"]
-    actual_members = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+    actual_members = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("_")]
     assert len(exected_members) == len(actual_members)
     for expected_member in exected_members:
         assert expected_member in actual_members
