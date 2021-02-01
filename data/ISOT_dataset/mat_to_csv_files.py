dc558e8c351abc9f7e78ca996726f50de31a4a1e
Justin_Morgan <2justinmorgan@gmail.com>
2021-01-31 22:55:31 -0800

    Writing user 2d lists to csv files BRANCH: research/mouse_data_feature_engineering_with_ISOT_dataset

--- data/ISOT_dataset/mat_to_csv_files.py
+++ data/ISOT_dataset/mat_to_csv_files.py
@@ -3,6 +3,8 @@ import sys
 import ntpath
 import csv
 import scipy.io
+import numpy as np
+
 
 def usage():
     sys.stderr.write(f"Usage: {ntpath.basename(__file__)} <mat_file>\n")
@@ -22,11 +24,31 @@ def check_args(argc, argv):
         usage()
         sys.exit(1)
 
-    return safe_open(argv[1])
+    return argv[1]
+
+
+def write_csv_files(users_dict):
+    for user_name in users_dict:
+        if type(users_dict[user_name]) != np.ndarray:
+            continue
+
+        print(f"{user_name}")
+        csv_file = open(f"{user_name}.csv", "w")
+        csv_file.write(f"type-of-action,traveled-distance,elapsed-time,direction-of-movement\n")
+        for row in users_dict[user_name]:
+            csv_file.write("%s," % row[0])
+            csv_file.write("%s," % row[1])
+            csv_file.write("%s," % row[2])
+            csv_file.write("%s" % row[3])
+            csv_file.write("\n")
 
 
 def main(argc, argv):
     mat_filepath = check_args(argc, argv)
+
+    users_dict = scipy.io.loadmat(mat_filepath)
+
+    write_csv_files(users_dict)
 
 
 if __name__ == "__main__":
